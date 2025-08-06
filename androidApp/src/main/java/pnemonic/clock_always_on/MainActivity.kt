04c21db6983a2f4f31066cd050c5c10c6826a941
Moshe W <pnemonic@gmail.com>
2025-08-06 15:48:00 +0300

    keep screen on.

--- androidApp/src/main/java/pnemonic/clock_always_on/MainActivity.kt
+++ androidApp/src/main/java/pnemonic/clock_always_on/MainActivity.kt
@@ -1,6 +1,7 @@
 package pnemonic.clock_always_on
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -27,6 +28,15 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        keepScreenOn()
+    }
+
+    private fun keepScreenOn() {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
 
